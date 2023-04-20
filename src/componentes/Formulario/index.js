701fb4c5b5d028264e193ea854e23c1b32bd41a6
andrezambon <andre-zambon@hotmail.com>
2023-04-20 05:36:01 -0300

    Cria lista suspensa no formulário

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,8 +1,20 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 
 const Formulario = () => {
+
+    //Cria a lista de times que é usada pela Lista Suspensa
+    const times = [ 
+        'Programação',
+        'Front-End',
+        'UX e Design', 
+        'DevOps',
+        'Back-End',
+        'Mobile'
+    ]
+
     return (
         <section className="formulario">
             <form>
@@ -10,6 +22,8 @@ const Formulario = () => {
                 <CampoTexto label='Nome' placeholder="Digite seu nome"/>
                 <CampoTexto label='Cargo' placeholder="Digite seu cargo"/>
                 <CampoTexto label='Imagem' placeholder="Digite a URL da imagem"/>
+                {/* Passa a lista de times como propriedade (props) para ser usada pelo componente Lista Suspensa */}
+                <ListaSuspensa label='Time' itens={times}/> 
             </form>
         </section>
     )
