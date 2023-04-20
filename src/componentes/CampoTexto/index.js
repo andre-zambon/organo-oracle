c1051dfcbb0d6d7db8dbc8b9912d1f625bbfb80c
andrezambon <andre-zambon@hotmail.com>
2023-04-20 04:28:55 -0300

    Formata css formulário

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,12 @@
 import './CampoTexto.css'
 
-const CampoTexto = () => {
+const CampoTexto = (props) => { //Recebe as propriedade (parâmetros informados no app.js) do objeto. pode usar qualquer nome para variável
     return (
-        <div className = "campo-texto">
-            <label>Nome</label>
-            <input placeholder="Digite seu nome"></input>
+         //Insere a classe no componente principal e a partir dele seleciona os filhos
+         <div className = "campo-texto"> 
+         {// acessa a propriedade label e usa o valor definido no parâmetro no app.js 
+            <label>{props.label}</label> }
+            <input placeholder={props.placeholder}></input>
         </div>
     )
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,11 +1,15 @@
+import CampoTexto from '../CampoTexto'
 import './Formulario.css'
 
-function Formulario () {
+
+const Formulario = () => {
     return (
-        <section>
+        <section className="formulario">
             <form>
-                
-                
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto label='Nome' placeholder="Digite seu nome"/>
+                <CampoTexto label='Cargo' placeholder="Digite seu cargo"/>
+                <CampoTexto label='Imagem' placeholder="Digite a URL da imagem"/>
             </form>
         </section>
     )
